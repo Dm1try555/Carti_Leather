7c6fb27a068f3a2cd31673e345e804d199ad1cbf
Dima <shilovscky2020@gmail.com>
2025-04-12 18:53:40 +0300

    add to main after feedback

--- users/admin.py
+++ users/admin.py
@@ -7,11 +7,20 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display = (
         'feedback_name',
         'feedback_email',
-        'feedback_message',
+        'short_message',
         'feedback_phone',
         'created_at',
     )
     ordering = ('-created_at',)
 
+    def short_message(self, obj):
+        """
+        Ограничивает длину сообщения до 100 символов.
+        """
+        if len(obj.feedback_message) > 100:
+            return obj.feedback_message[:100] + '...'
+        return obj.feedback_message
+
+    short_message.short_description = 'Повідомлення'
 
 admin.site.register(Feedback, FeedbackAdmin)
